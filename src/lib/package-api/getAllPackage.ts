7c8fc6e5c1c0181233afa634adda59a61bc71fe4
insanity31 <riveratrishamaee@gmail.com>
2025-03-24 18:29:39 +0800

    CHANGES

--- src/lib/package-api/getAllPackage.ts
+++ src/lib/package-api/getAllPackage.ts
@@ -1,80 +1,117 @@
 import BASE_URL from "../../util/BaseUrl";
 
-interface Division {
-    divisionId: number;
-    division: string;
-    title: string;
-    sdsName: string;
-    sdsPosition: string;
-    itoName: string;
-    itoEmail: string;
-}
-
-interface District {
-    districtId: number;
-    division: Division;
-    name: string;
-}
-
-interface School {
-    schoolRecordId: number;
-    district: District;
-    classification: string;
-    schoolId: string;
-    name: string;
-    address: string;
-    previousStation: string;
+interface Package {
+  packageId: Package;
+  item: string;
+  status: string;
+  component: string;
+  serialNumber: string;
+  assigned: string;
+  remarks: string;
+  configuration: Configuration;
+  schoolBatchList: SchoolBatchList;
 }
 
 interface SchoolBatchList {
-    schoolBatchId: number;
-    school: School;
-    deliveryDate: string;
-    numberOfPackage: number;
-    status: string;
-    keyStage: string;
-    remarks: string;
-    accountable: string;
+  batch: Batch;
+  school: School;
+  deliveryDate: string;
+  numberOfPackages: string;
+  component: string;
+  serialNumber: string;
+  assigned: string;
+  remarks: string;
+  package: Package;
+}
+
+interface School {
+  division: Division;
+  district: District;
+  classification?: string;
+  schoolId?: string;
+  name: string;
+  address?: string;
+  landline?: string;
+  schoolHead?: string;
+  schoolHeadNumber?: string;
+  schoolHeadEmail?: string;
+  propertyCustodian?: string;
+  propertyCustodianNumber?: string;
+  propertyCustodianEmail?: string;
+  energized?: boolean;
+  energizedRemarks?: boolean;
+  localGridSupply?: boolean;
+  connectivity?: boolean;
+  smart?: boolean;
+  globe?: boolean;
+  digitalNetwork?: boolean;
+  am?: boolean;
+  fm?: boolean;
+  tv?: boolean;
+  cable?: boolean;
+  ntcRemark?: string;
+  designation?: string;
+  previousStation?: string;
+}
+
+interface Division {
+  divisionId: number;
+  division: string;
+  title: string;
+  sdsName: string;
+  sdsPosition: string;
+  itoName: string;
+  itoEmail: string;
+}
+
+interface District {
+  districtId: number;
+  name: string;
+  division: Division;
+}
+
+interface Batch {
+  batchId: number;
+  batchName: string;
+  batchYear: string;
+  deliveryYear: string;
+  supplier: string;
+  numberOfPackage: string;
+  remarks: string;
+  configuration: Configuration;
 }
 
 interface Configuration {
-    configurationId: number;
-    item: string;
-    type: string;
-    quantity: number;
+  configurationId: number;
+  item: string;
+  type: string;
+  quantity: number;
 }
 
-interface Package {
-    packageId: number;
-    schoolBatchList: SchoolBatchList;
-    configuration: Configuration;
-    status: string;
-    component: string;
-    serialNumber: string;
-    assigned: string;
-    remarks: string;
-}
+export const getAllPackage = async (): Promise<Package[]> => {
+  try {
+    const response = await fetch(`${BASE_URL}/package/get_all`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-export const getPackages = async (): Promise<Package[]> => {
-    try {
-        const response = await fetch(`${BASE_URL}/package/get_all`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-
-        if (!response.ok) {
-            throw new Error("Failed to fetch packages");
-        }
-
-        const responseData = await response.json();
-
-        const data: Package[] = responseData;
-
-        return data;
-    } catch (error) {
-        console.error("Error fetching packages:", error);
-        throw error;
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(
+        `Failed to fetch packages. Status: ${response.status}, Message: ${errorMessage}`
+      );
     }
+
+    const responseData = await response.json();
+
+    const data: Package[] = responseData.data;
+
+    console.log("Fetched Packages:", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching packages:", error);
+    throw error;
+  }
 };
--- src/lib/schoolbatchlist-api/getAllSchoolBatchList.ts
+++ src/lib/schoolbatchlist-api/getAllSchoolBatchList.ts
@@ -1,105 +1,119 @@
 import BASE_URL from "../../util/BaseUrl";
 
-interface Batch {
-    batchId: number;
-    batchName: string;
-    budgetYear: string;
-    deliveryYear: string;
-    price: string;
-    supplier: string;
-    numberOfPackage: string;
-    remarks: string;
-    configurations: Configuration[];
-}
-
 interface SchoolBatchList {
-    schoolBatchId: number;
-    batch: Batch;
-    school: School;
-    deliveryDate: number;
-    numberOfPackage: number;
-    status: string;
-    keyStage: string;
-    remarks: string;
-    accountable: string;
-    packages: Package[];
+  schoolBatchId: SchoolBatchList;
+  batch: Batch;
+  school: School;
+  deliveryDate: string;
+  numberOfPackages: string;
+  component: string;
+  serialNumber: string;
+  assigned: string;
+  remarks: string;
+  package: Package;
 }
 
 interface School {
-    schoolRecordId: number;
-    district: District;
-    classification: string;
-    schoolId: string;
-    name: string;
-    address: string;
-    previousStation: string;
-}
-
-interface District {
-    districtId: number;
-    name: string;
-    division: Division;
+  division: Division;
+  district: District;
+  classification?: string;
+  schoolId?: string;
+  name: string;
+  address?: string;
+  landline?: string;
+  schoolHead?: string;
+  schoolHeadNumber?: string;
+  schoolHeadEmail?: string;
+  propertyCustodian?: string;
+  propertyCustodianNumber?: string;
+  propertyCustodianEmail?: string;
+  energized?: boolean;
+  energizedRemarks?: boolean;
+  localGridSupply?: boolean;
+  connectivity?: boolean;
+  smart?: boolean;
+  globe?: boolean;
+  digitalNetwork?: boolean;
+  am?: boolean;
+  fm?: boolean;
+  tv?: boolean;
+  cable?: boolean;
+  ntcRemark?: string;
+  designation?: string;
+  previousStation?: string;
 }
 
 interface Division {
-    divisionId: number;
-    division: string;
-    title: string;
-    sdsName: string;
-    sdsPosition: string;
-    itoName: string;
-    itoEmail: string;
-}
-
-interface Package {
-    packageId: number;
-    item: string;
-    status: string;
-    component: string;
-    serialNumber: string;
-    assigned: string;
-    remarks: string;
+  divisionId: number;
+  division: string;
+  title: string;
+  sdsName: string;
+  sdsPosition: string;
+  itoName: string;
+  itoEmail: string;
 }
 
 interface District {
-    districtId: number;
-    name: string;
-    division: Division;
+  districtId: number;
+  name: string;
+  division: Division;
+}
+
+interface Batch {
+  batchId: number;
+  batchName: string;
+  batchYear: string;
+  deliveryYear: string;
+  supplier: string;
+  numberOfPackage: string;
+  remarks: string;
+  configuration: Configuration;
+}
+
+interface Package {
+  packageId: number;
+  item: string;
+  status: string;
+  component: string;
+  serialNumber: string;
+  assigned: string;
+  remarks: string;
+  configuration: Configuration;
 }
 
 interface Configuration {
-    configurationId: number;
-    item: string;
-    type: string;
-    quantity: number;
+  configurationId: number;
+  item: string;
+  type: string;
+  quantity: number;
+  batch: Batch;
 }
 
 export const getAllSchoolBatchLists = async (): Promise<SchoolBatchList[]> => {
-    try {
-        const response = await fetch(`${BASE_URL}/school_batch_list/get_all`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+  try {
+    const response = await fetch(`${BASE_URL}/school_batch_list/get_all`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (!response.ok) {
-            const errorMessage = await response.text();
-            throw new Error(
-                `Failed to fetch school batch lists. Status: ${response.status}, Message: ${errorMessage}`
-            );
-        }
-
-        const responseData = await response.json();
-
-        console.error(responseData);
-
-        const data: SchoolBatchList[] = responseData.data;
-
-        console.log("Fetched School Batch Lists:", data);
-        return data;
-    } catch (error) {
-        console.error("Error fetching school batch lists:", error);
-        throw error;
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(
+        `Failed to fetch schoolbatchlists. Status: ${response.status}, Message: ${errorMessage}`
+      );
     }
+
+    const responseData = await response.json();
+
+    // Extract the data array from the response
+    const data: SchoolBatchList[] = responseData.data;
+
+    console.log("Fetched SchoolBatchLists:", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching schoolbatchlists:", error);
+    throw error;
+  }
 };
