97f37f2ce8e769226951576db8744117c0808706
insanity31 <riveratrishamaee@gmail.com>
2025-03-24 18:28:38 +0800

    CHANGES

--- src/lib/schoolbatchlist-api/deleteSchoolBatchList.ts
+++ src/lib/schoolbatchlist-api/deleteSchoolBatchList.ts
@@ -1,30 +1,27 @@
 import BASE_URL from "../../util/BaseUrl";
 
 export const deleteSchoolBatchListById = async (
-    id: number
+  id: number
 ): Promise<boolean> => {
-    try {
-        const response = await fetch(
-            `${BASE_URL}/school_batch_list/delete/${id}`,
-            {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
+  try {
+    const response = await fetch(`${BASE_URL}/school_batch_list/delete/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (!response.ok) {
-            const errorMessage = await response.text();
-            throw new Error(
-                `Failed to delete school batch list. Status: ${response.status}, Message: ${errorMessage}`
-            );
-        }
-
-        console.log(`School Batch List with ID ${id} deleted successfully.`);
-        return true;
-    } catch (error) {
-        console.error("Error deleting school batch list:", error);
-        return false;
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(
+        `Failed to delete school batch list. Status: ${response.status}, Message: ${errorMessage}`
+      );
     }
+
+    console.log(`School Batch List with ID ${id} deleted successfully.`);
+    return true;
+  } catch (error) {
+    console.error("Error deleting school batch list:", error);
+    return false;
+  }
 };
