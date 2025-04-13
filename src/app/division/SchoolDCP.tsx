1cdc9ffd45d55cf6e15e17b8c911e13631b52f8e
Acrylus <theantonjoseph@gmail.com>
2025-04-13 15:34:52 +0800

    DATE to YEAR

--- src/app/division/SchoolDCP.tsx
+++ src/app/division/SchoolDCP.tsx
@@ -836,13 +836,7 @@ const SchoolDCP = () => {
                                                 {sbl.batch.batchName}
                                             </td>
                                             <td className="px-4 py-2 border border-slate-300">
-                                                {sbl.deliveryDate
-                                                    ? new Date(
-                                                          sbl.deliveryDate
-                                                      ).toLocaleDateString(
-                                                          "en-CA"
-                                                      )
-                                                    : "N/A"}
+                                                {sbl.deliveryDate}
                                             </td>
                                             <td className="px-4 py-2 border border-slate-300">
                                                 {sbl.numberOfPackage}
